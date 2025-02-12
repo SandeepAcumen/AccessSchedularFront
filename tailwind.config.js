770f3b31b81d69a29507da548ab7943d1c64b19e
preethi <preethi.acumenvelocity@gmail.com>
2025-02-12 10:29:57 +0530

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#1976d2",
+        hover: "#0d47a1",
+        active: "#1E40AF",
+        border: "#2563EB",
+        background: "#f6f7f8",
+        primaryText: "#FFFFFF",
+        cardBg: "#bbdefb",
+        gradientColorFrom: '#0B73A2',
+        gradientColorTo: '#57A8DF',
+      },
+    },
   },
   plugins: [],
 }
