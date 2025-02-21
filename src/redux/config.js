660a68bb80970b3fad8b53a67c3c6e82303a0cb7
SandeepAcumen <sandeepkumarg.acumenusa@gmail.com>
2025-02-21 12:17:27 +0530

    Config file changes

--- src/redux/config.js
+++ src/redux/config.js
@@ -1,2 +1,2 @@
-export const url = "http://localhost:3600/api/";
+export const url = "http://localhost:3000/api/";
 export const googleClientId = "818247973279-fnppb9ffge0odmrtie9gid4q392asdlf.apps.googleusercontent.com"
